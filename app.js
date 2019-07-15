56d9c9ff90341848fe9461a927185f450b0cb57e
Matt Elliott <mattelliottus@gmail.com>
2019-07-14 19:58:11 -0700

    set static folder path

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 (function() {
 const express = require('express');
 const app = express();
-const vue = require('vue');
+const vue = require('ejs');
 const port = 1111;
 
+//set express to look at static files from assets folder
+app.use('/assets/', express.static('./assets/'));
 //set view as template engine
-// app.set('views', './Views');
-app.set('view engine', pug);
+app.set('views', './Views');
+app.set('view engine', 'ejs');
 //set up routes
 app.get('/', function(req,res) {
-  res.render('index.html');
+  res.render('index');
+});
+
+app.get('/assets/*', function(req,res) {
+  res.send(res);
 })
 
 //start server
