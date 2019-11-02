fc45e464c2115960363f6252fb8b05fee87fdb4c
Matt <mattelliottus@gmail.com>
2019-11-02 00:28:52 -0700

    fixed sanitizing from client

--- utils/messageSlack.js
+++ utils/messageSlack.js
@@ -2,12 +2,14 @@ module.exports = (input) => {
   const Slack = require('slack-node');
   let slack = new Slack();
 
-  slack.setWebhook('https://hooks.slack.com/services/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');
+  slack.setWebhook('https://hooks.slack.com/services/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');
 
   //TODO: move and improve sanitizing into a factory
-  input.replace('%40', '@');
-
-  let data = '*New Contact*\n' + input.split('&').join('\n');
+  let data = '*New Contact*\n' +
+    input.replace(/%40/g, '@')
+      .replace(/%20/, ' ')
+      .split('&')
+      .join('\n');
   
   slack.webhook({
     channel: 'new-business',
