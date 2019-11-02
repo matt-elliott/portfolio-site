9075eec4ff944fd8fd1979f9b458d6bb710fe3c5
Matt <mattelliottus@gmail.com>
2019-11-02 00:13:24 -0700

    installed and required slack-node, enabled slack to receive json data, added api route for contact, added util to send message to slack and required it, cleaned up routes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const messageSlack = require('./utils/messageSlack');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //set express to look at static files from assets folder
 app.use('/assets/', express.static('./assets/'));
 //set view as template engine
@@ -13,14 +18,19 @@ app.get('/', function(req,res) {
   res.render('index');
 });
 
-app.get('/portfolio', function(req,res) {
+app.get('/portfolio', (req,res) => {
   res.render('portfolio');
 });
 
-app.get('/contact', function(req,res) {
+app.get('/contact', (req,res) => {
   res.render('contact');
 });
 
+app.post('/api/contact', (req, res) => {
+  // console.log('new api request to /api/contact: ',req.body);
+  messageSlack(req.body.data);
+  res.end();
+});
 
 //start server
 app.listen(port, function() {
